001aff4452ea9f702fb4b1ad3b5c8342f43365ac
Andrew Fleming <michiganknight2@gmail.com>
2025-05-28 00:16:18 -0500

    Rename Globals.txt to Globals.js

---
+++ NodeJS/Globals.js
@@ -0,0 +1,13 @@
+// GLOBALS - NO WINDOW
+
+// __dirname  - Path to current directory
+// __filename - File name
+// require    - Function to use modules (CommonJS)
+// module     - Info about current module (File)
+// process    - Info about environment where the program is being executed
+
+console.log(__dirname)
+
+setInterval(() => {
+    console.log('Hello World')
+}, 1000);
