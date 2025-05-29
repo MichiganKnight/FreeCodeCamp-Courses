47952cb2d001109a34e8f106ab4ce1cd348b0ff2
Andrew Fleming <michiganknight2@gmail.com>
2025-05-29 16:27:28 -0500

    Add More Complex Modules

--- NodeJS/Modules.js
+++ NodeJS/Modules.js
@@ -3,7 +3,15 @@
 
 const names = require('./Names')
 const sayHi = require('./Utils')
+const data = require('./Alternative Syntax')
+
+require('./Mind Grenade')
+
+/* console.log(data)
+console.log(data.items)
+console.log(data.items[0] + ' ' + data.items[1])
+console.log(data.singlePerson.name)
 
 sayHi('Drew')
 sayHi(names.john)
-sayHi(names.peter)
+sayHi(names.peter) */
