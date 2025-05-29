088fe6cc67594313fd4c2ac49204e4b50a9cfdff
Andrew Fleming <michiganknight2@gmail.com>
2025-05-29 16:38:33 -0500

    Update 3 - Modules.js

--- NodeJS/3 - Modules.js
+++ NodeJS/3 - Modules.js
@@ -7,11 +7,11 @@ const data = require('./6 - Alternative Syntax')
 
 require('./7 - Mind Grenade')
 
-/* console.log(data)
+console.log(data)
 console.log(data.items)
 console.log(data.items[0] + ' ' + data.items[1])
 console.log(data.singlePerson.name)
 
 sayHi('Drew')
 sayHi(names.john)
-sayHi(names.peter) */
+sayHi(names.peter)
