3e7e7fea2c40a1c3a099c1dbd0eab33342a762cd
Andrew Fleming <michiganknight2@gmail.com>
2025-05-30 11:52:24 -0500

    Sync Updates

--- NodeJS/10 - FS Sync.js
+++ NodeJS/10 - FS Sync.js
@@ -1,8 +1,13 @@
 const { readFileSync, writeFileSync } = require('fs')
 
+console.log('Start')
+
 const first = readFileSync('./Content/First.txt', 'utf8')
 const second = readFileSync('./Content/Second.txt', 'utf8')
 
 console.log(first, second)
 
 writeFileSync('./Content/Result-Sync.txt', `Result: ${first}, ${second}`, { flag: 'a' })
+
+console.log('Task Completed')
+console.log('Starting Next Task')
