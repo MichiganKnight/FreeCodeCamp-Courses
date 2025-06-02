7ed14e8442aae9e5e5c3484ae6c96e163e170752
Andrew Fleming <michiganknight2@gmail.com>
2025-06-02 17:17:29 -0500

    Update Server.js & Ignore Environment File

--- MERN Stack/Web Application/Backend/Server.js
+++ MERN Stack/Web Application/Backend/Server.js
@@ -2,6 +2,10 @@ import express from "express"
 
 const app = express();
 
+app.get("/Products", (req, res) => {
+    
+})
+
 app.listen(5000, () => {
     console.log("Server Started at http://localhost:5000")
 })
