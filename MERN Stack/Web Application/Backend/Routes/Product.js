e093d40ad1eea4a88f02ce9066eca253d5090003
Andrew Fleming <michiganknight2@gmail.com>
2025-06-02 18:19:52 -0500

    Update Product.js

--- MERN Stack/Web Application/Backend/Routes/Product.js
+++ MERN Stack/Web Application/Backend/Routes/Product.js
@@ -5,11 +5,8 @@ import { createProduct, deleteProduct, getProducts, updateProduct } from "../Con
 const router = express.Router()
 
 router.get("/", getProducts)
-
 router.post("/", createProduct)
-
 router.put("/:id", updateProduct)
-
 router.delete("/:id", deleteProduct)
 
 export default router
