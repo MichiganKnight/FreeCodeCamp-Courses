85cc0a822027f09f0f57b941fb7f0febcf9b1295
Andrew Fleming <michiganknight2@gmail.com>
2025-06-02 17:44:03 -0500

    Update Server.js

--- MERN Stack/Web Application/Backend/Server.js
+++ MERN Stack/Web Application/Backend/Server.js
@@ -1,14 +1,42 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./Config/DB.js"
+import Product from "./Models/Product.js"
 
 dotenv.config()
 
 const app = express()
 
-app.get("/Products", (req, res) => {
-    
+app.post("/API/Products", async (req, res) => {
+    const product = req.body // User Sends Data
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({
+            success: false,
+            message: "Please Provide All Fields"
+        })
+    }
+
+    const newProduct = new Product(product)
+
+    try {
+        await newProduct.save()
+
+        res.status(201).json({
+            success: true,
+            data: newProduct
+        })
+    } catch (error) {
+        console.log(`Error Creating Product: ${error.message}`)
+
+        res.status(500).json({
+            sucess: false,
+            message: "Server Error"
+        })
+    }
 })
+
+// Postman Desktop App
 
 app.listen(5000, () => {
     connectDB()
