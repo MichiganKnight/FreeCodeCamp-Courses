06312825feb2d5309ee4940751134201c970fc0c
Andrew Fleming <michiganknight2@gmail.com>
2025-06-02 20:41:50 -0500

    Update CreatePage.jsx

--- MERN Stack/Web Application/Frontend/src/Pages/CreatePage.jsx
+++ MERN Stack/Web Application/Frontend/src/Pages/CreatePage.jsx
@@ -21,9 +21,9 @@ const CreatePage = () => {
 
                 <Box w={"full"} bg={useColorModeValue("white", "gray.800")} p={6} rounded={"lg"} shadow={"md"}>
                     <VStack spacing={4}>
-                        <Input placeholder='Product Name' name='name' value={newProduct.name} onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value})} />
-                        <Input placeholder='Price' name='price' value={newProduct.price} onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value})} />
-                        <Input placeholder='Image' name='image' value={newProduct.image} onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value})} />
+                        <Input placeholder='Product Name' name='name' value={newProduct.name} onChange={(e) => setNewProduct({ ...newProduct, name: e.target.value })}/>
+						<Input placeholder='Price' name='price' type='number' value={newProduct.price} onChange={(e) => setNewProduct({ ...newProduct, price: e.target.value })}/>
+						<Input placeholder='Image URL' name='image' value={newProduct.image} onChange={(e) => setNewProduct({ ...newProduct, image: e.target.value })}/>
 
                         <Button colorScheme='blue' onClick={handleAddProduct} w='full'>Add Product</Button>
                     </VStack>
