f0cafeaa7baf7c1d21fd0dda0633e35e5216fdc3
Andrew Fleming <michiganknight2@gmail.com>
2025-06-02 20:22:11 -0500

    Add Various Pages

--- MERN Stack/Web Application/Frontend/src/App.jsx
+++ MERN Stack/Web Application/Frontend/src/App.jsx
@@ -1,10 +1,19 @@
-import { Button } from "@chakra-ui/react"
+import { Box, useColorModeValue } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
+import CreatePage from "./Pages/CreatePage"
+import HomePage from "./Pages/HomePage"
+import Navbar from "./Components/Navbar"
 
 function App() {
   return (
-    <>
-      <Button>No</Button>
-    </>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")}>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/Create" element={<CreatePage />} ></Route>
+      </Routes>
+    </Box>
   )
 }
 
