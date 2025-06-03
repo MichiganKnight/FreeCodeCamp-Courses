f8d2ab1498313e713f995d8e4a575f1db9e2769e
Andrew Fleming <michiganknight2@gmail.com>
2025-06-02 22:17:46 -0500

    Update Server.js

--- MERN Stack/Web Application/Backend/Server.js
+++ MERN Stack/Web Application/Backend/Server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import path from "path"
 import { connectDB } from "./Config/DB.js"
 import productRoutes from "./Routes/Product.js"
 
@@ -8,8 +9,17 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 5000
 
+const __dirname = path.resolve()
+
 app.use(express.json()) // Allows JSON Data in req.body
 app.use("/API/Products", productRoutes)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/Frontend/dist")))
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"))
+    })
+}
 
 app.listen(PORT, () => {
     connectDB()
