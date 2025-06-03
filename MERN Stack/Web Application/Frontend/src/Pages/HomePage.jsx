edf386862a1eb2e33f0ce1cd5fbc153e281d6198
Andrew Fleming <michiganknight2@gmail.com>
2025-06-02 21:33:12 -0500

    Update HomePage.jsx

--- MERN Stack/Web Application/Frontend/src/Pages/HomePage.jsx
+++ MERN Stack/Web Application/Frontend/src/Pages/HomePage.jsx
@@ -20,15 +20,16 @@ const HomePage = () => {
                     Current Products 🚀
                 </Text>
 
-                <SimpleGrid columns={{
-                    base: 1,
-                    md: 2,
-                    lg: 3
-                }} spacing={10} w={"full"}>
-                    {products.map((product) => {
-                        <ProductCard key={product._id} product={product} />
-                    })}
-                </SimpleGrid>
+                <SimpleGrid
+					columns={{
+						base: 1,
+						md: 2,
+						lg: 3,
+					}} spacing={10} w={"full"}>
+					{products.map((product) => (
+						<ProductCard key={product._id} product={product} />
+					))}
+				</SimpleGrid>
 
                 <Text fontSize={"xl"} textAlign={"center"} fontWeight={"bold"} color={"gray.500"}>
                     No Products Found 😢 {" "}
