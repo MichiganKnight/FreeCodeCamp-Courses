e74f0078556d3d0f4a5d63b059eea48c28c8bf17
Andrew Fleming <michiganknight2@gmail.com>
2025-06-03 18:33:06 -0500

    Update Message.js

--- MERN Stack/Chat App/Backend/Controllers/Message.js
+++ MERN Stack/Chat App/Backend/Controllers/Message.js
@@ -27,6 +27,7 @@ export const sendMessage = async (req, res) => {
             conversation.messages.push(newMessage._id)
         }
 
+        // Runs in Parallel
         await Promise.all([conversation.save(), newMessage.save()])
 
         res.status(201).json(newMessage)
