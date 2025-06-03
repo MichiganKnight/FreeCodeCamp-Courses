03e30fc8ac28e4517bae89d4be52da157d6966ed
Andrew Fleming <michiganknight2@gmail.com>
2025-06-03 16:45:20 -0500

    Update Server.js

--- MERN Stack/Chat App/Backend/Server.js
+++ MERN Stack/Chat App/Backend/Server.js
@@ -1,12 +1,17 @@
-const express = require("express")
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express()
+
+const PORT = process.env.PORT || 5000
 
 app.get("/", (req, res) => {
     // Root Route - http://localhost:5000
     res.send("Hello World")
 })
 
-app.listen(5000, () => {
-    console.log("Server Running on Port 5000")
+app.listen(PORT, () => {
+    console.log(`Server Running on Port: ${PORT}`)
 })
