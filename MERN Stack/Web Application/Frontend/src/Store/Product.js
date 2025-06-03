768fa6e0e610d0a2bcacb042564573591f9f66de
Andrew Fleming <michiganknight2@gmail.com>
2025-06-02 22:04:46 -0500

    Added Update Functions

--- MERN Stack/Web Application/Frontend/src/Store/Product.js
+++ MERN Stack/Web Application/Frontend/src/Store/Product.js
@@ -61,5 +61,33 @@ export const useProductStore = create((set) => ({
             success: true,
             message: data.message
         }
+    },
+    updateProduct: async (pid, updatedProduct) => {
+        const res = await fetch(`/API/Products/${pid}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(updatedProduct)
+        })
+
+        const data = await res.json()
+
+        if (!data.success) {
+            return {
+                success: false,
+                message: data.message
+            }
+        }
+
+        // Update UI Immediately
+        set((state) => ({
+            products: state.products.map((product) => (product._id === pid ? data.data: product))
+        }))
+
+        return {
+            success: true,
+            message: data.message
+        }
     }
 }))
