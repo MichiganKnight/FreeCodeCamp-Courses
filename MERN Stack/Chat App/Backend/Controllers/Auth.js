d4015bf9b44e259b81a72d87e69db54a85a9c799
Andrew Fleming <michiganknight2@gmail.com>
2025-06-03 17:29:00 -0500

    Update User Model Backend

--- MERN Stack/Chat App/Backend/Controllers/Auth.js
+++ MERN Stack/Chat App/Backend/Controllers/Auth.js
@@ -1,8 +1,50 @@
+import User from "../Models/User.js"
+
 export const signup = async (req, res) => {
     try {
         const { fullName, username, password, confirmPassword, gender } = req.body
-    } catch (error) {
 
+        if (password !== confirmPassword) {
+            return res.status(400).json({
+                error: "Passords Don't Match"
+            })
+        }
+
+        const user = await User.findOne({ username })
+
+        if (user) {
+            return res.status(400).json({
+                error: "Username Already Exists"
+            })
+        }
+
+        // HASH PASSWORD HERE
+
+        const maleProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const femaleProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+        const newUser = new User({
+            fullName,
+            username,
+            password,
+            gender,
+            profilePic: gender === "male" ? maleProfilePic : femaleProfilePic
+        })
+
+        await newUser.save()
+
+        res.status(201).json({
+            _id: newUser._id,
+            fullName: newUser.fullName,
+            username: newUser.username,
+            profilePic: newUser.profilePic
+        })
+    } catch (error) {
+        console.log(`Error During Signup: ${error.message}`)
+        
+        res.status(500).json({
+            error: "Internal Server Error"
+        })
     }
 }
 
--- MERN Stack/Chat App/Backend/Server.js
+++ MERN Stack/Chat App/Backend/Server.js
@@ -4,17 +4,18 @@ import dotenv from "dotenv"
 import authRoutes from "./Routes/Auth.js"
 import connectToMongoDB from "./DB/ConnectToDB.js"
 
-dotenv.config()
-
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.get("/", (req, res) => {
+dotenv.config()
+
+app.use(express.json()) // Pase Incoming Requests with JSON
+app.use("/API/Auth", authRoutes)
+
+/* app.get("/", (req, res) => {
     // Root Route - http://localhost:5000
     res.send("Hello World")
-})
-
-app.use("/API/Auth", authRoutes)
+}) */
 
 app.listen(PORT, () => {
     connectToMongoDB()
