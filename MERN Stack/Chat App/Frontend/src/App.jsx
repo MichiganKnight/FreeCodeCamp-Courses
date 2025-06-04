938d3085c2ba5cac5930cfa270065c4fa097af62
Andrew Fleming <michiganknight2@gmail.com>
2025-06-04 11:56:01 -0500

    Home Page & Placeholder Messages

--- MERN Stack/Chat App/Frontend/src/App.jsx
+++ MERN Stack/Chat App/Frontend/src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Login from './Pages/Login/Login'
 import SignUp from './Pages/Signup/Signup'
+import Home from './Pages/Home/Home'
 
 function App() {
   return (
     <div className='p-4 h-screen flex items-center justify-center'>
-      <SignUp />
+      <Home />
     </div>
   )
 }
