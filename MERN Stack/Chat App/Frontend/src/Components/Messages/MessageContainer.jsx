356af4bb8ab15857071425b2a5f469d4416fcec4
Andrew Fleming <michiganknight2@gmail.com>
2025-06-04 12:03:56 -0500

    Send Message Functionality

--- MERN Stack/Chat App/Frontend/src/Components/Messages/MessageContainer.jsx
+++ MERN Stack/Chat App/Frontend/src/Components/Messages/MessageContainer.jsx
@@ -1,3 +1,4 @@
+import MessageInput from "./MessageInput"
 import Messages from "./Messages"
 
 const MessageContainer = () => {
@@ -10,6 +11,7 @@ const MessageContainer = () => {
                 </div>
 
                 <Messages />
+                <MessageInput />
             </>
         </div>
     )
