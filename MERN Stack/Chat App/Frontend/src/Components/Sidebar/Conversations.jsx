f5a9ad59e3d6eb2030c0f1762d255fce1a51617a
Andrew Fleming <michiganknight2@gmail.com>
2025-06-04 16:42:05 -0500

    Added Sidebar Functions
    Users showing as "online" vs "offline" is currently broken. Not sure why

--- MERN Stack/Chat App/Frontend/src/Components/Sidebar/Conversations.jsx
+++ MERN Stack/Chat App/Frontend/src/Components/Sidebar/Conversations.jsx
@@ -1,14 +1,17 @@
+import useGetConversations from "../../Hooks/UseGetConversations"
 import Conversation from "./Conversation"
+import { getRandomEmoji } from "../../Utils/Emojis"
 
 const Conversations = () => {
+    const { loading, conversations } = useGetConversations()
+
     return (
         <div className="py-2 flex flex-col overflow-auto">
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
-            <Conversation />
+            {conversations.map((conversation, idx) => (
+                <Conversation key={conversation._id} conversation={conversation} emoji={getRandomEmoji()} lastIdx={idx === conversations.length - 1} />
+            ))}
+
+            {loading ? <span className="loading loading-spinner"></span> : null}
         </div>
     )
 }
