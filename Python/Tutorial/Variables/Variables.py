e4e191590dfac2a2baaae13a94a9723f92433020
Andrew Fleming <michiganknight2@gmail.com>
2025-06-10 17:21:01 -0500

    Update Variables.py

--- Python/Tutorial/Variables/Variables.py
+++ Python/Tutorial/Variables/Variables.py
@@ -1,4 +1,16 @@
-name = "Drew"
-age = 26
+# This is a Comment
 
-print(f"Name: {name} | Age: {age}")
+name = "Drew" # Inline Comment
+age = 26
+age2 = 26.6
+#age3 = float(26.6) - Not Using
+#age4 = int("26") - Cast int from str
+
+print(f"Name: {name} - Age: {age}")
+print(f"Name: {type(name)} - Age: {type(age)}\n")
+
+print(f"type(name) Example - {type(name) == str}")
+print(f"isinstance(name, str) Example - {isinstance(name, str)}\n")
+
+print(f"Age: {age} | {type(age)} - {isinstance(age, int)}")
+print(f"Age: {age2} | {type(age2)} - {isinstance(age2, float)}\n")
