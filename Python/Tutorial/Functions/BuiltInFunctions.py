06fc511c0d517018c08a2cdb5b75bdeb2de1d862
Andrew Fleming <michiganknight2@gmail.com>
2025-06-10 18:10:56 -0500

    Create BuiltInFunctions.py

---
+++ Python/Tutorial/Functions/BuiltInFunctions.py
@@ -0,0 +1,6 @@
+print("=== Built-In Functions ===")
+
+print(abs(-5.5)) # Absolute Value
+print(round(5.5)) # Round to Nearest Integer
+print(round(5.49))
+print(round(5.49, 1)) # Round to 1 Decimal Point
