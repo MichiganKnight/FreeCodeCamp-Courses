b3d726aec9e7544d924081b5852e4b4ca56fe3cd
Andrew Fleming <michiganknight2@gmail.com>
2025-06-10 01:17:42 -0500

    Add Convex

--- Full Stack/codemeet/src/app/page.tsx
+++ Full Stack/codemeet/src/app/page.tsx
@@ -1,13 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
-      <Button>
-        Sign In
-      </Button>
+    <div className="m-10">
+      Home Page
     </div>
   );
 }
