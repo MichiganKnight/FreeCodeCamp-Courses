72d41e673e982780666fb026ac96505bf3ec5363
Andrew Fleming <michiganknight2@gmail.com>
2025-06-12 15:33:40 -0500

    Create Objects.py
    Not sure the difference with lists?

---
+++ Python/Tutorial/Objects/Objects.py
@@ -0,0 +1,13 @@
+print("=== Objects ===")
+
+age = 26
+
+print(age.real)
+print(age.imag)
+print(age.bit_length())
+
+items = [1, 2]
+items.append(3)
+items.pop()
+
+print(id(items)) # Location in memory
