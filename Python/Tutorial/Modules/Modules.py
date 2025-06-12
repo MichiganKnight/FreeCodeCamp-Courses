8661d7042ee639a8221dee53b7e213c4c442d298
Andrew Fleming <michiganknight2@gmail.com>
2025-06-12 15:57:42 -0500

    Add Modules

---
+++ Python/Tutorial/Modules/Modules.py
@@ -0,0 +1,30 @@
+print("=== Modules ===")
+
+#import Dog # Load all functions
+#from Lib.Dog import bark # Import individual functions
+#from Lib import Dog # Import from subfolder
+from Lib.Dog import bark
+
+#import math
+from math import sqrt
+
+#Dog.bark()
+#bark()
+#Dog.bark()
+
+bark()
+
+#print(math.sqrt(4))
+print(sqrt(4))
+
+# math for Math Utilities
+# re for Regular Expressions
+# json for JSON
+# datetime for Dates
+# sqlite3 for SQLite
+# os for Operating System
+# random for Random Number Generation
+# statistics for Statistics
+# requests for HTTP Network Requests
+# http for HTTP Servers
+# urllib for URLs
