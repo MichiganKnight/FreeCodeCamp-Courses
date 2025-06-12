71f28737147213ca4ab6ff13612afb23512233a2
Andrew Fleming <michiganknight2@gmail.com>
2025-06-12 13:05:03 -0500

    Update Dictionaries.py

--- Python/Tutorial/Data Types/Dictionaries.py
+++ Python/Tutorial/Data Types/Dictionaries.py
@@ -1,1 +1,39 @@
-# 1:54:03
+print("=== Dictionaries ===")
+
+# Key: # Value
+
+dog = { "name": "Drew", "age": 26, "color": "red" }
+
+print(dog["name"])
+
+dog["age"] = 27
+
+print(dog)
+print(dog.get("name"))
+print(dog.get("color", "red")) # Red is default value
+print("color" in dog)
+print(len(dog))
+
+dog["favorite food"] = "Meat"
+print(dog)
+
+del dog["color"]
+print(dog)
+
+dog_copy = dog.copy()
+print(dog_copy)
+
+print("\n=== Get Keys & Values ===")
+
+print(dog.keys())
+print(list(dog.keys()))
+print(dog.values())
+print(list(dog.values()))
+print(dog.items())
+print(list(dog.items()))
+
+print("\n=== Pop Functions ===")
+
+print(dog.popitem()) # Remove last item added
+print(dog.pop("name")) # Return and delete item
+print(dog)
