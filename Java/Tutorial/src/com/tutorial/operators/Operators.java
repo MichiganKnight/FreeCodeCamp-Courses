fc139f38bde96d168dbb124fda1a26063cb361c8
Andrew Fleming <michiganknight2@gmail.com>
2025-06-14 02:45:57 -0500

    Create Operators.java

---
+++ Java/Tutorial/src/com/tutorial/operators/Operators.java
@@ -0,0 +1,26 @@
+package com.tutorial.operators;
+
+public class Operators {
+    public static void main(String[] args) {
+        System.out.println("=== Operators ===");
+
+        int number1 = 12;
+        int number2 = 6;
+
+        // Addition
+        //System.out.println(number1 + " + " + number2 + " = " + (number1 + number2));
+        System.out.println(number1 + number2);
+
+        // Subtraction
+        System.out.println(number1 - number2);
+
+        // Multiplication
+        System.out.println(number1 * number2);
+
+        // Division
+        System.out.println(number1 / number2);
+
+        // Modulus
+        System.out.println(number1 % number2);
+    }
+}
