522c74cefcb0c1ed93cdf949cd46180c98cd5be8
Andrew Fleming <michiganknight2@gmail.com>
2025-06-16 12:51:47 -0500

    Create Sprite Class

--- Java/Mario/src/main/java/components/SpriteRenderer.java
+++ Java/Mario/src/main/java/components/SpriteRenderer.java
@@ -7,20 +7,15 @@ import renderer.Texture;
 
 public class SpriteRenderer extends Component {
     private Vector4f color;
-    private Vector2f texCoords;
-    // (0, 1) Bottom Right
-    // (0, 0) Bottom Left
-    // (1, 1) Top Right
-    // (1, 0) Top Left
-    private Texture texture;
+    private Sprite sprite;
 
     public SpriteRenderer(Vector4f color) {
         this.color = color;
-        this.texture = null;
+        this.sprite = new Sprite(null);
     }
 
-    public SpriteRenderer(Texture texture) {
-        this.texture = texture;
+    public SpriteRenderer(Sprite sprite) {
+        this.sprite = sprite;
         this.color = new Vector4f(1, 1, 1, 1);
     }
 
@@ -39,17 +34,10 @@ public class SpriteRenderer extends Component {
     }
 
     public Texture getTexture() {
-        return this.texture;
+        return sprite.getTexture();
     }
 
     public Vector2f[] getTexCoords() {
-        Vector2f[] texCoords = {
-                new Vector2f(1, 1),
-                new Vector2f(1, 0),
-                new Vector2f(0, 0),
-                new Vector2f(0, 1)
-        };
-
-        return texCoords;
+        return sprite.getTexCoords();
     }
 }
--- Java/Mario/src/main/java/jade/LevelEditorScene.java
+++ Java/Mario/src/main/java/jade/LevelEditorScene.java
@@ -1,8 +1,8 @@
 package jade;
 
+import components.Sprite;
 import components.SpriteRenderer;
 import org.joml.Vector2f;
-import org.joml.Vector4f;
 import util.AssetPool;
 
 public class LevelEditorScene extends Scene {
@@ -15,7 +15,7 @@ public class LevelEditorScene extends Scene {
         this.camera = new Camera(new Vector2f(-250, 0));
 
         GameObject obj1 = new GameObject("Object 1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
-        obj1.addComponent(new SpriteRenderer(AssetPool.getTexture("Assets/Images/TestImage.png")));
+        obj1.addComponent(new SpriteRenderer(new Sprite(AssetPool.getTexture("Assets/Images/TestImage.png"))));
         this.addGameObjectToScene(obj1);
 
         loadResources();
