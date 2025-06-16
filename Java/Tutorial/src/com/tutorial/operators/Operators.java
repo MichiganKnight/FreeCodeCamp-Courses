4cbc1c79df2a8b05455560ac95c6c2e76148f17a
Andrew Fleming <michiganknight2@gmail.com>
2025-06-16 13:31:31 -0500

    Update Operators.java

--- Java/Tutorial/src/com/tutorial/operators/Operators.java
+++ Java/Tutorial/src/com/tutorial/operators/Operators.java
@@ -4,23 +4,23 @@ public class Operators {
     public static void main(String[] args) {
         System.out.println("=== Arithmatic Operators ===");
 
-        int number1 = 12;
-        int number2 = 6;
+        double number1 = 12;
+        double number2 = 5;
 
         // Addition
         //System.out.println(number1 + " + " + number2 + " = " + (number1 + number2));
-        System.out.println(number1 + number2);
+        System.out.println("Addition: " + (number1 + number2));
 
         // Subtraction
-        System.out.println(number1 - number2);
+        System.out.println("Subtraction: " + (number1 - number2));
 
         // Multiplication
-        System.out.println(number1 * number2);
+        System.out.println("Multiplication: " + (number1 * number2));
 
         // Division
-        System.out.println(number1 / number2);
+        System.out.println("Division: " + (number1 / number2));
 
         // Modulus
-        System.out.println(number1 % number2);
+        System.out.println("Modulus: " + (number1 % number2));
     }
 }
