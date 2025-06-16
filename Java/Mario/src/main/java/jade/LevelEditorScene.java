4a5bac4b72c3d532eab2363cf7630df5960b6cfc
Andrew Fleming <michiganknight2@gmail.com>
2025-06-16 13:28:30 -0500

    Running Mario

--- Java/Mario/src/main/java/jade/LevelEditorScene.java
+++ Java/Mario/src/main/java/jade/LevelEditorScene.java
@@ -1,12 +1,14 @@
 package jade;
 
-import components.Sprite;
 import components.SpriteRenderer;
 import components.Spritesheet;
 import org.joml.Vector2f;
 import util.AssetPool;
 
 public class LevelEditorScene extends Scene {
+    private GameObject obj1;
+    private Spritesheet sprites;
+
     public LevelEditorScene() {
 
     }
@@ -17,9 +19,9 @@ public class LevelEditorScene extends Scene {
 
         this.camera = new Camera(new Vector2f(-250, 0));
 
-        Spritesheet sprites = AssetPool.getSpritesheet("Assets/Images/Spritesheet.png");
+        sprites = AssetPool.getSpritesheet("Assets/Images/Spritesheet.png");
 
-        GameObject obj1 = new GameObject("Object 1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
+        obj1 = new GameObject("Object 1", new Transform(new Vector2f(100, 100), new Vector2f(256, 256)));
         obj1.addComponent(new SpriteRenderer(sprites.getSprite(0)));
         this.addGameObjectToScene(obj1);
 
@@ -34,17 +36,27 @@ public class LevelEditorScene extends Scene {
         AssetPool.addSpritesheet("Assets/Images/Spritesheet.png", new Spritesheet(AssetPool.getTexture("Assets/Images/Spritesheet.png"), 16, 16, 26, 0));
     }
 
+    private int spriteIndex = 0;
+    private float spriteFlipTime = 0.2f;
+    private float spriteFlipTimeLeft = 0.0f;
+
     @Override
     public void update(float dt) {
         System.out.println("FPS: " + (1.0f / dt));
 
-        if (dt > 0.1f) {
-            dt = 0.1f;
-        }
+        spriteFlipTimeLeft -= dt;
 
-        // Square Movement
-        //camera.position.x -= dt * 50.0f;
-        //camera.position.y -= dt * 20.0f;
+        if (spriteFlipTimeLeft <= 0) {
+            spriteFlipTimeLeft = spriteFlipTime;
+
+            spriteIndex++;
+
+            if (spriteIndex > 4) {
+                spriteIndex = 0;
+            }
+
+            obj1.getComponent(SpriteRenderer.class).setSprite(sprites.getSprite(spriteIndex));
+        }
 
         for (GameObject go : this.gameObjects) {
             go.update(dt);
