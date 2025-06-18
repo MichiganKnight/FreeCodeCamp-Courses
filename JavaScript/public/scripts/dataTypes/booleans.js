bf2648dfbf78ae9260959a83a1804da8a7208bd6
Andrew Fleming <michiganknight2@gmail.com>
2025-06-18 16:34:32 -0500

    Update booleans.js

--- JavaScript/public/scripts/dataTypes/booleans.js
+++ JavaScript/public/scripts/dataTypes/booleans.js
@@ -5,3 +5,12 @@ function welcomeToBooleans() {
 }
 
 console.log(welcomeToBooleans());
+
+console.log("\n=== Return Booleans ===");
+
+function isLess(a, b) {
+    return a < b;
+}
+
+console.log(isLess(10, 15));
+console.log(isLess(20, 15));
