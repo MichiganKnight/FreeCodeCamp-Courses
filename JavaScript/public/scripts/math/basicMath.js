a896b0e2407490c9db4cbb288da6649fd1f449fe
Andrew Fleming <michiganknight2@gmail.com>
2025-06-18 15:08:54 -0500

    Create basicMath.js

---
+++ JavaScript/public/scripts/math/basicMath.js
@@ -0,0 +1,20 @@
+console.log("=== Basic Math ===");
+
+var sum = 10 + 10;
+var difference = 45 - 0;
+var product = 8 * 10;
+var quotient = 66 / 33;
+
+console.log(sum);
+console.log(difference);
+console.log(product);
+console.log(quotient);
+
+var myVar = 87;
+var myVar2 = 11;
+
+myVar++;
+myVar2--;
+
+console.log(myVar);
+console.log(myVar2);
