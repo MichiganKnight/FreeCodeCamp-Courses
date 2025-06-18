3d6b5748f5db3b5fa51bf33b8b4e397c52ca9f72
Andrew Fleming <michiganknight2@gmail.com>
2025-06-18 16:03:15 -0500

    Create if.js

---
+++ JavaScript/public/scripts/conditionalStatements/if.js
@@ -0,0 +1,12 @@
+console.log("=== If Statements ===");
+
+function trueOrFalse(wasThatTrue) {
+    if (wasThatTrue) {
+        return "Yes - True";
+    } else {
+        return "No - False";
+    }
+}
+
+console.log(trueOrFalse(true));
+console.log(trueOrFalse(false));
