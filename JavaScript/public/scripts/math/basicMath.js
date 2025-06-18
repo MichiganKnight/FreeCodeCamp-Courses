cb8db5bbb0ade510988e1a840b34a4b6b8e953e1
Andrew Fleming <michiganknight2@gmail.com>
2025-06-18 15:10:32 -0500

    Update basicMath.js

--- JavaScript/public/scripts/math/basicMath.js
+++ JavaScript/public/scripts/math/basicMath.js
@@ -18,3 +18,16 @@ myVar2--;
 
 console.log(myVar);
 console.log(myVar2);
+
+var ourDecimal = 5.7;
+var myDecimal = 0.009;
+
+var product = 2.0 * 2.5;
+var quotient = 4.4 / 2.0;
+
+console.log(product);
+console.log(quotient);
+
+var remainder = 11 % 3;
+
+console.log(remainder);
