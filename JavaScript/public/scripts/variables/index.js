514c3be3a097fb80f3fc911db5ddc8f42b23a277
Andrew Fleming <michiganknight2@gmail.com>
2025-06-18 14:00:33 -0500

    Convert Into Node.js Environment

--- JavaScript/public/scripts/variables/index.js
+++ JavaScript/public/scripts/variables/index.js
@@ -1,5 +1,9 @@
 // In-line Comment
 
+/*
+    Multi-Line Comment
+*/
+
 var number = 5;
 
 console.log(number);
---
+++ JavaScript/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const fs = requrie('fs');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+const scriptsDir = path.join(__dirname, 'public', 'scripts');
+
+app.use(express.static('public'));
+
+app.get('/api/scripts', (req, res) => {
+    const scriptsList = [];
+
+    const walk = (dir, relPath = '') => {
+        const files = fs.readdirSync(dir);
+        files.forEach(file => {
+            const fullPath = path.join(dir, file);
+            const relFilePath = path.join(relPath, file);
+
+            if (fs.statSync(fullPath).isDirectory()) {
+                walk(fullPath, relFilePath);
+            } else if (file.endsWith('.js')) {
+                const label = path.basename(file, '.js').replace(/([A-Z])/g, ' $1').replace(/^./, c => c.toUpperCase()) + ' Program';
+                scriptsList.push({ label, path: `scripts/${relFilePath.replace(/\\/g, '/')}` });
+            }
+        });
+    };
+
+    walk(scriptsDir);
+    res.json(scriptsList);
+});
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server Running at http://localhost:${PORT}`);
+});
