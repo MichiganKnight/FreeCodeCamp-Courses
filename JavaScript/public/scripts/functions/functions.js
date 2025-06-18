86a575ffccb2a8d7ba3ae6686ab43c766f8d3d16
Andrew Fleming <michiganknight2@gmail.com>
2025-06-18 15:59:44 -0500

    Update functions.js

--- JavaScript/public/scripts/functions/functions.js
+++ JavaScript/public/scripts/functions/functions.js
@@ -87,3 +87,17 @@ function processArg(num) {
 processed = processArg(7);
 
 console.log(processed);
+
+console.log("\n=== Stand In Line ===");
+
+function nextInLine(arr, item) {
+    arr.push(item);
+
+    return arr.shift();
+}
+
+var testArr = [1, 2, 3, 4, 5];
+
+console.log(`Before: ${JSON.stringify(testArr)}`);
+console.log(nextInLine(testArr, 6));
+console.log(`After: ${JSON.stringify(testArr)}`);
