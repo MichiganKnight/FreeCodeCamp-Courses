d27431c45e5ae2c20d49411e5321623fa8e33b00
Andrew Fleming <michiganknight2@gmail.com>
2025-06-19 17:38:10 -0500

    Update operators.js

--- JavaScript/public/scripts/operators/operators.js
+++ JavaScript/public/scripts/operators/operators.js
@@ -339,3 +339,26 @@ function checkSum(num) {
 console.log(checkSum(10));
 console.log(checkSum(-10));
 console.log(checkSum(0));
+
+console.log("\n=== Rest Operator ===");
+
+const sum = (function() {
+    return function sum(...args) {
+        return args.reduce((a, b) => a + b, 0);
+    };
+})();
+
+console.log(sum(1, 2, 3, 4));
+
+console.log("\n=== Spread Operator ===");
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+
+let arr2;
+
+(function() {
+    arr2 = [...arr1];
+    arr1[0] = 'Potato';
+})();
+
+console.log(arr2);
