d58cf18c952700cf941a37c8ece4d33c1ab8e612
Andrew Fleming <michiganknight2@gmail.com>
2025-06-18 22:08:23 -0500

    Create objects.js

---
+++ JavaScript/public/scripts/objects/objects.js
@@ -0,0 +1,31 @@
+console.log("=== Objects ===");
+
+var myDog = {
+    "Name": "Dog",
+    "Legs": 3,
+    "Tails": 2,
+    "Friends": []
+};
+
+console.log(myDog);
+
+console.log("\n=== Dot Notation ===");
+
+var testObj = {
+    "Hat": "Baseball Hat",
+    "Shirt": "T-Shirt",
+    "Shoes": "Sandals"
+};
+
+console.log(testObj.Hat);
+
+console.log("\n=== Bracket Notation ===");
+
+var testObj = {
+    "An Entree": "Hamburger",
+    "My Side": "Veggies",
+    "The Drink": "Water"
+};
+
+console.log(testObj["An Entree"]);
+console.log(testObj["The Drink"]);
