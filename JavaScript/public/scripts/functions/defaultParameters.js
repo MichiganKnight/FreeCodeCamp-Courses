e5b6c7954028f97eaabec02c21bdeaa62f6f06d2
Andrew Fleming <michiganknight2@gmail.com>
2025-06-19 17:35:15 -0500

    Create defaultParameters.js

---
+++ JavaScript/public/scripts/functions/defaultParameters.js
@@ -0,0 +1,10 @@
+console.log("=== Default Parameters ===");
+
+const increment = (function() {
+    return function increment(number, value = 1) {
+        return number + value;
+    };
+})();
+
+console.log(increment(5, 2));
+console.log(increment(5));
