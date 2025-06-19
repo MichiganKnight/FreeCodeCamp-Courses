5ab9dc176d979ef8df9228cb0951ed459a5db870
Andrew Fleming <michiganknight2@gmail.com>
2025-06-19 17:03:33 -0500

    Create doWhileLoops.js

---
+++ JavaScript/public/scripts/loops/doWhileLoops.js
@@ -0,0 +1,16 @@
+console.log("=== Do While Loops ===");
+
+// Run At Least Once
+
+var myArray = [];
+var i = 10;
+
+do {
+    myArray.push(i);
+
+    i++;
+} while (i < 5) {
+
+}
+
+console.log(i, myArray);
