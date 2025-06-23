89ffcb8d1c12f0568a87acdb8425a316c0c51d9d
Andrew Fleming <michiganknight2@gmail.com>
2025-06-23 13:34:19 -0500

    Update Advanced Select Query.sql

--- SQL/Advanced Select Query.sql
+++ SQL/Advanced Select Query.sql
@@ -18,3 +18,23 @@ LIMIT 2;
 SELECT Student_Name, Student_Major
 FROM Student
 WHERE Student_Major = 'Chemistry' OR Student_Name = 'Kate';
+
+SELECT Student_Name, Student_Major
+FROM Student
+WHERE Student_Major <> 'Chemistry';
+
+SELECT *
+FROM Student
+WHERE Student_ID <= 3 AND Student_Name <> 'Jack';
+
+SELECT *
+FROM Student
+WHERE Student_Name IN ('Claire', 'Kate', 'Mike');
+
+SELECT *
+FROM Student
+WHERE Student_Major IN ('Biology', 'Chemistry');
+
+SELECT *
+FROM Student
+WHERE Student_Major IN ('Biology', 'Chemistry') AND Student_ID > 2;
