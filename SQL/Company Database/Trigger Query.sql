8cb1ba690b5e57a5d07815d412833e56ee55bd21
Andrew Fleming <michiganknight2@gmail.com>
2025-06-23 16:21:20 -0500

    Update Trigger Query.sql

--- SQL/Company Database/Trigger Query.sql
+++ SQL/Company Database/Trigger Query.sql
@@ -7,10 +7,38 @@ CREATE
 	TRIGGER MyTrigger BEFORE INSERT
     ON Employee
     FOR EACH ROW BEGIN
-		INSERT INTO Trigger_Test VALUES ('Added New Employye');
+		INSERT INTO Trigger_Test VALUES ('Added New Employee');
 	END$$
 DELIMITER ;
 
 INSERT INTO Employee VALUES (109, 'Oscar', 'Martinez', '1968-02-19', 'M', 69000, 106, 3);
 
 SELECT * FROM Trigger_Test;
+
+DELIMITER $$
+CREATE
+	TRIGGER MyTrigger1 BEFORE INSERT
+    ON Employee
+    FOR EACH ROW BEGIN
+		INSERT INTO Trigger_Test VALUES (NEW.First_Name);
+	END$$
+DELIMITER ;
+
+INSERT INTO Employee VALUES (110, 'Kevin', 'Malone', '1978-02-19', 'M', 69000, 106, 3);
+
+SELECT * FROM Trigger_Test;
+
+DELIMITER $$
+CREATE
+	TRIGGER MyTrigger2 BEFORE INSERT
+    ON Employee
+	FOR EACH ROW BEGIN
+		IF NEW.Sex = 'M' Then
+			INSERT INTO Trigger_Test VALUES ('Added Male Employee');
+		ELSEIF NEW.Sex = 'F' Then
+			INSERT INTO Trigger_Test VALUES ('Added Female Employee');
+		ELSE
+			INSERT INTO Trigger_Test VALUES ('Added Other Employee');
+		END IF;
+	END$$
+DELIMITER $$;
