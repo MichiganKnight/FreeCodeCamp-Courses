9ffba74c51c148f026162200714dc1a70b68fa82
Andrew Fleming <michiganknight2@gmail.com>
2025-06-23 13:08:21 -0500

    Add SQL Scripts

---
+++ SQL/Create Table Query.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Student (
+	Student_ID INT AUTO_INCREMENT,
+    Student_Name VARCHAR(20) NOT NULL DEFAULT 'John Doe',
+    Student_Major VARCHAR(20) NOT NULL DEFAULT 'Undecided',
+    PRIMARY KEY (Student_ID)
+);
