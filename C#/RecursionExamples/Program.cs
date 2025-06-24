bdfc98a45c9646b3e2e6e85ab127fbb956bf688b
Andrew Fleming <michiganknight2@gmail.com>
2025-06-24 16:34:18 -0500

    Updates

--- C#/RecursionExamples/Program.cs
+++ C#/RecursionExamples/Program.cs
@@ -4,7 +4,21 @@
     {
         public static void Main(string[] args)
         {
+            string binaryNumber = DecimalToBinary(233, "");
             
+            Console.WriteLine(binaryNumber);
+        }
+
+        private static string DecimalToBinary(int number, string result)
+        {
+            if (number == 0)
+            {
+                return result;
+            }
+            
+            result = number % 2 + result;
+
+            return DecimalToBinary(number / 2, result);
         }
     }   
 }
