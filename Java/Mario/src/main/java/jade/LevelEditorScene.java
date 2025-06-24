428aa73db5938bfc3b453598f6a4713d2c3d90ac
Andrew Fleming <michiganknight2@gmail.com>
2025-06-23 20:30:41 -0500

    Update LevelEditorScene.java

--- Java/Mario/src/main/java/jade/LevelEditorScene.java
+++ Java/Mario/src/main/java/jade/LevelEditorScene.java
@@ -5,6 +5,7 @@ import components.SpriteRenderer;
 import components.Spritesheet;
 import imgui.ImGui;
 import org.joml.Vector2f;
+import org.joml.Vector4f;
 import util.AssetPool;
 
 public class LevelEditorScene extends Scene {
@@ -24,7 +25,7 @@ public class LevelEditorScene extends Scene {
         sprites = AssetPool.getSpritesheet("Assets/Images/Spritesheet.png");
 
         obj1 = new GameObject("Object 1", new Transform(new Vector2f(200, 100), new Vector2f(256, 256)), 2);
-        obj1.addComponent(new SpriteRenderer(new Sprite(AssetPool.getTexture("Assets/Images/BlendImage1.png"))));
+        obj1.addComponent(new SpriteRenderer(new Vector4f(1, 0 , 0, 1)));
         this.addGameObjectToScene(obj1);
         this.activeGameObject = obj1;
 
