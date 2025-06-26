686d1bf481d690eee69cef78d9d50a3cd0355c5e
Andrew Fleming <michiganknight2@gmail.com>
2025-06-26 14:41:17 -0500

    Add StudyGuide.html

--- Revature Prep/JavaPrep/src/main/java/Main.java
+++ Revature Prep/JavaPrep/src/main/java/Main.java
@@ -1,7 +1,19 @@
 package main.java;
 
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
+        String htmlPath = System.getProperty("user.dir") + "/src/main/StudyGuide.html";
 
+        File htmlFile = new File(htmlPath);
+
+        try {
+            Desktop.getDesktop().browse(htmlFile.toURI());
+        } catch (IOException e) {
+            System.out.println("Error Opening File: " + e.getMessage());
+        }
     }
 }
