fad636eb572d094ee905ba65d6228d3211e63bfa
Andrew Fleming <michiganknight2@gmail.com>
2025-06-26 14:50:01 -0500

    Various Refactoring Changes

--- Revature Prep/JavaPrep/src/main/java/Main.java
+++ Revature Prep/JavaPrep/src/main/java/Main.java
@@ -3,16 +3,24 @@ package main.java;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) {
-        String htmlPath = System.getProperty("user.dir") + "/src/main/StudyGuide.html";
+        URL htmlResource = Main.class.getClassLoader().getResource("StudyGuide.html");
 
-        File htmlFile = new File(htmlPath);
+        if (htmlResource == null) {
+            System.out.println("Error: StudyGuide.html Not Found");
+
+            return;
+        }
 
         try {
+            File htmlFile = Paths.get(htmlResource.toURI()).toFile();
             Desktop.getDesktop().browse(htmlFile.toURI());
-        } catch (IOException e) {
+        } catch (IOException | URISyntaxException e) {
             System.out.println("Error Opening File: " + e.getMessage());
         }
     }
