eacac10c6b5ee81f3a55a15cf5cacc4fa2824831
Andrew Fleming <michiganknight2@gmail.com>
2025-06-26 17:33:40 -0500

    Various Minor Changes

--- Revature Prep/JavaPrep/src/main/java/classesandobjects/Dog.java
+++ Revature Prep/JavaPrep/src/main/java/classesandobjects/Dog.java
@@ -1,4 +1,4 @@
-package main.java;
+package main.java.classesandobjects;
 
 public class Dog {
     String breed;
---
+++ Revature Prep/JavaPrep/src/main/java/constructors/Constructors.java
@@ -0,0 +1,5 @@
+package main.java.constructors;
+
+public class Constructors {
+
+}
