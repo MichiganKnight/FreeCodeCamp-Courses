4f5a6ae53eb82382485b966959dd97d3de9cf0fd
Andrew Fleming <michiganknight2@gmail.com>
2025-07-02 13:15:13 -0500

    Create RunnableThreadExample.java

---
+++ Java/Multithreading/src/main/java/basicMultithreading/RunnableThreadExample.java
@@ -0,0 +1,36 @@
+package main.java.basicMultithreading;
+
+public class RunnableThreadExample {
+    public static void main(String[] args) {
+        Thread threadOne = new Thread(new ThreadOne());
+        Thread threadTwo = new Thread(new ThreadTwo());
+
+        Thread threadThree = new Thread(() -> {
+            for (int i = 0; i < 15; i ++) {
+                System.out.println("Thread Three: " + i);
+            }
+        });
+
+        threadOne.start();
+        threadTwo.start();
+        threadThree.start();
+    }
+}
+
+class ThreadOne implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i ++) {
+            System.out.println("Thread One: " + i);
+        }
+    }
+}
+
+class ThreadTwo implements Runnable {
+    @Override
+    public void run() {
+        for (int i = 0; i < 15; i ++) {
+            System.out.println("Thread Two: " + i);
+        }
+    }
+}
