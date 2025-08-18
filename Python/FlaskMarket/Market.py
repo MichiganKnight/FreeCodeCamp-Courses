44d54513d83f7c87dde0d336460be81a00333d72
Andrew Fleming <michiganknight2@gmail.com>
2025-08-18 16:32:26 -0500

    Updates

--- Python/FlaskMarket/Market.py
+++ Python/FlaskMarket/Market.py
@@ -1,6 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/Home')
+@app.route('/home')
+@app.route('/Index')
+@app.route('/index')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/About/<username>')
+def about(username):
+    return f'<h1>About Page of {username}</h1>'
