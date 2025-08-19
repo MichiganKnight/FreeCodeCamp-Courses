a7e57dc263ddd7fc89b5fea9dc729f1c8a7eb205
Andrew Fleming <michiganknight2@gmail.com>
2025-08-19 16:16:01 -0500

    Add Market Page

--- Python/FlaskMarket/Market.py
+++ Python/FlaskMarket/Market.py
@@ -8,8 +8,13 @@ app = Flask(__name__)
 @app.route('/Index')
 @app.route('/index')
 def home_page():
-    return render_template('home.html')
+    return render_template('Home.html')
 
 @app.route('/About/<username>')
 def about(username):
     return f'<h1>About Page of {username}</h1>'
+
+@app.route('/Market')
+@app.route('/market')
+def market_page():
+    return render_template('Market.html')
