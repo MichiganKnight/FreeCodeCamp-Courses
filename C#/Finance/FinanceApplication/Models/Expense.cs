e5d41e7730367cc00dad72083ae28ac8ddbf9686
Andrew Fleming <michiganknight2@gmail.com>
2025-08-20 17:21:05 -0500

    Update Expense.cs

--- C#/Finance/FinanceApplication/Models/Expense.cs
+++ C#/Finance/FinanceApplication/Models/Expense.cs
@@ -1,7 +1,20 @@
-﻿namespace FinanceApplication.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FinanceApplication.Models
 {
     public class Expense
     {
+        public int Id { get; set; }
         
+        [Required]
+        public string? Description { get; set; }
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Amount Must Be Greater Than 0")]
+        public double Amount { get; set; }
+        [Required]
+        public string? Category { get; set; }
+        
+        
+        public DateTime Date { get; set; } = DateTime.Now;
     }
 }
