eeef0c94a9ca3cd6ef8814cfc717ad29bf1276b8
Andrew Fleming <michiganknight2@gmail.com>
2025-08-20 17:35:43 -0500

    Create FinanceAppContext.cs

---
+++ C#/Finance/FinanceApplication/Data/FinanceAppContext.cs
@@ -0,0 +1,15 @@
+﻿using FinanceApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceApplication.Data
+{
+    public class FinanceAppContext : DbContext
+    {
+        public FinanceAppContext(DbContextOptions<FinanceAppContext> options) : base(options)
+        {
+            
+        }
+        
+        DbSet<Expense> Expenses { get; set; }
+    }
+}
