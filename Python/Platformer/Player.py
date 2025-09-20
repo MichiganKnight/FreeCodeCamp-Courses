99a6f2e4eb6969d1369cd4b8410351fc4abeaa76
Andrew Fleming <michiganknight2@gmail.com>
2025-09-19 23:08:39 -0500

    Add Player Animation

--- Python/Platformer/Player.py
+++ Python/Platformer/Player.py
@@ -6,8 +6,11 @@ from Utils import load_sprite_sheets
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0)
     GRAVITY = 1
+    ANIMATION_DELAY = 3
 
     def __init__(self, x, y, width, height):
+        super().__init__()
+
         self.SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True)
 
         self.sprite = None
@@ -40,9 +43,26 @@ class Player(pygame.sprite.Sprite):
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1
+        self.update_sprite()
+
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprite_index = (self.animation_count // self.ANIMATION_DELAY) % len(sprites)
+
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
+        self.update()
+
+    def update(self):
+        self.rect = self.sprite.get_rect(topLeft = (self.rect.x, self.rect.y))
+        self.mask = pygame.mask.from_surface(self.sprite)
 
     def draw(self, win):
-        self.sprite = self.SPRITES["idle_" + self.direction][0]
         win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 def handle_move(player):
