68b73863147e2660b4e44cbeb74a362ef52f0a01
Andrew Fleming <michiganknight2@gmail.com>
2025-09-20 07:34:30 -0500

    Add Various Assets

--- Python/Mario/Level.py
+++ Python/Mario/Level.py
@@ -1,6 +1,6 @@
 import pygame
 
-from Settings import tile_size
+from Settings import tile_size, screen_width
 
 from Tiles import Tile
 from Player import Player
@@ -28,12 +28,39 @@ class Level:
                     player_sprite = Player((col_index * tile_size, row_index * tile_size))
                     self.player.add(player_sprite)
 
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if player_x < screen_width / 4 and direction_x < 0:
+            self.world_shift = 8
+            player.speed = 0
+        elif player_x > screen_width - (screen_width / 4) and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
+
+    def horizontal_movement_collision(self):
+        player = self.player.sprite
+        player.rect.x += player.direction.x * player.speed
+
+        for sprite in self.tiles.sprites():
+            if sprite.rect.colliderect(player.rect):
+                if player.direction.x < 0:
+                    player.rect.left = sprite.rect.right
+                elif player.direction.x > 0:
+                    player.rect.right = sprite.rect.left
 
     def run(self):
         # Level Tiles
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+        self.scroll_x()
 
         # Player
         self.player.update()
+        self.horizontal_movement_collision()
         self.player.draw(self.display_surface)
