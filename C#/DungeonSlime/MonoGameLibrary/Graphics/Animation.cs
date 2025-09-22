b7499b2c10d2b0da1b94c8d320812c8a29a639eb
Andrew Fleming <michiganknight2@gmail.com>
2025-09-22 12:06:56 -0500

    Create Animation.cs

---
+++ C#/DungeonSlime/MonoGameLibrary/Graphics/Animation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MonoGameLibrary.Graphics
+{
+    public class Animation
+    {
+        /// <summary>
+        /// Texture Regions that Make Up the Frames of the Animation
+        /// Order of the Region Within the Collection are the Order that the Frames are Played
+        /// </summary>
+        public List<TextureRegion> Frames { get; set; }
+        
+        /// <summary>
+        /// Amount of Time to Delay Between Frames
+        /// </summary>
+        public TimeSpan Delay { get; set; }
+
+        /// <summary>
+        /// Creates a new Animation
+        /// </summary>
+        public Animation()
+        {
+            Frames = [];
+            Delay = TimeSpan.FromMilliseconds(100);
+        }
+        
+        /// <summary>
+        /// Creates a new Animation with the Specified Frames and Delay
+        /// </summary>
+        /// <param name="frames">Ordered Collection of the Frames for this Animation</param>
+        /// <param name="delay">Amount of Time to Delay Each Frame of this Animation</param>
+        public Animation(List<TextureRegion> frames, TimeSpan delay)
+        {
+            Frames = frames;
+            Delay = delay;
+        }
+    }
+}
