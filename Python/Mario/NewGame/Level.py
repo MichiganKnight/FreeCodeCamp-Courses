93fafe2f9eaa0f996c1843c743ee79698edb2bb7
Andrew Fleming <michiganknight2@gmail.com>
2025-09-21 22:16:19 -0500

    Add Static Tiles

--- Python/Mario/NewGame/Level.py
+++ Python/Mario/NewGame/Level.py
@@ -1,13 +1,18 @@
 import pygame
 
+from os.path import join
 from NewGame.Settings import tile_size
-from NewGame.Tiles import Tile
-from Utils import import_csv_layout
+from NewGame.Tiles import Tile, StaticTile
+from Utils import *
 
 class Level:
     def __init__(self, level_data, surface):
-        self.display_surface = surface
 
+        # General Setup
+        self.display_surface = surface
+        self.world_shift = 0
+
+        # Terrain Setup
         terrain_layout = import_csv_layout(level_data['Terrain'])
         self.terrain_sprites = self.create_tile_group(terrain_layout, 'Terrain')
 
@@ -21,7 +26,10 @@ class Level:
                     y = row_index * tile_size
 
                     if group_type == 'Terrain':
-                        sprite = Tile(tile_size, x, y)
+                        terrain_tile_list = import_cut_graphics(join('NewGame', 'Assets', 'Terrain', 'terrain_tiles.png'))
+                        tile_surface = terrain_tile_list[int(val)]
+
+                        sprite = StaticTile(tile_size, x, y, tile_surface)
                         sprite_group.add(sprite)
 
         return sprite_group
@@ -29,4 +37,4 @@ class Level:
     def run(self):
         # Run Entire Game
         self.terrain_sprites.draw(self.display_surface)
-        self.terrain_sprites.update(-4)
+        self.terrain_sprites.update(self.world_shift)
--- Python/Mario/NewGame/Utils.py
+++ Python/Mario/NewGame/Utils.py
@@ -1,4 +1,7 @@
 from csv import reader
+from NewGame.Settings import tile_size
+
+import pygame
 
 def import_csv_layout(path):
     terrain_map = []
@@ -10,3 +13,21 @@ def import_csv_layout(path):
         for row in level:
             terrain_map.append(list(row))
         return terrain_map
+
+def import_cut_graphics(path):
+    surface = pygame.image.load(path).convert_alpha()
+    tile_num_x = int(surface.get_width() / tile_size)
+    tile_num_y = int(surface.get_height() / tile_size)
+
+    cut_tiles = []
+    for row in range(tile_num_y):
+        for col in range(tile_num_x):
+            x = col * tile_size
+            y = row * tile_size
+
+            new_surface = pygame.Surface((tile_size, tile_size))
+            new_surface.blit(surface, (0, 0), pygame.Rect(x, y, tile_size, tile_size))
+
+            cut_tiles.append(new_surface)
+
+    return cut_tiles
