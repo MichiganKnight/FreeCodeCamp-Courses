a4d69ea1abe3e5c6471cda376c3196aab01b915c
Andrew Fleming <michiganknight2@gmail.com>
2025-09-23 20:40:43 -0500

    Spring Example Java Project

---
+++ Java/Spring Apps/Runnerz/src/test/java/dev/drew/runnerz/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.drew.runnerz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
