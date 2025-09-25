f807308d3f108de3a3f9497c120c0391bfb7d649
Andrew Fleming <michiganknight2@gmail.com>
2025-09-25 15:16:29 -0500

    Basic Tiles

---
+++ Python/PlatformerRemastered/Data/Tilesets/Objects.tsx
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Objects" tilewidth="156" tileheight="186" tilecount="28" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="46" height="50" source="../../Graphics/Objects/Barrel.png"/>
+ </tile>
+ <tile id="1">
+  <image width="128" height="128" source="../../Graphics/Objects/BG_Palm_1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="50" height="36" source="../../Graphics/Objects/Crate.png"/>
+ </tile>
+ <tile id="3">
+  <image width="68" height="186" source="../../Graphics/Objects/Flag.png"/>
+ </tile>
+ <tile id="4">
+  <image width="78" height="136" source="../../Graphics/Objects/Large_1.png"/>
+ </tile>
+ <tile id="5">
+  <image width="156" height="44" source="../../Graphics/Objects/Ship.png"/>
+ </tile>
+ <tile id="6">
+  <image width="80" height="103" source="../../Graphics/Objects/Small_1.png"/>
+ </tile>
+ <tile id="7">
+  <image width="74" height="56" source="../../Graphics/Objects/Player.png"/>
+ </tile>
+ <tile id="8">
+  <image width="76" height="46" source="../../Graphics/Objects/Shell.png"/>
+ </tile>
+ <tile id="9">
+  <image width="48" height="46" source="../../Graphics/Objects/Tooth.png"/>
+ </tile>
+ <tile id="10">
+  <image width="64" height="64" source="../../Graphics/Objects/Floor_Spikes.png"/>
+ </tile>
+ <tile id="11">
+  <image width="64" height="64" source="../../Graphics/Objects/Spike_Ball.png"/>
+ </tile>
+ <tile id="12">
+  <image width="104" height="106" source="../../Graphics/Objects/BG_Palm_Left.png"/>
+ </tile>
+ <tile id="13">
+  <image width="104" height="106" source="../../Graphics/Objects/BG_Palm_Right.png"/>
+ </tile>
+ <tile id="14">
+  <image width="42" height="32" source="../../Graphics/Objects/BG_Crate.png"/>
+ </tile>
+ <tile id="15">
+  <image width="64" height="64" source="../../Graphics/Objects/Candle.png"/>
+ </tile>
+ <tile id="16">
+  <image width="64" height="64" source="../../Graphics/Objects/Window.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="116" source="../../Graphics/Objects/Big_Chain.png"/>
+ </tile>
+ <tile id="18">
+  <image width="64" height="62" source="../../Graphics/Objects/Small_Chain.png"/>
+ </tile>
+ <tile id="19">
+  <image width="14" height="34" source="../../Graphics/Objects/Green_Bottle1.png"/>
+ </tile>
+ <tile id="20">
+  <image width="30" height="14" source="../../Graphics/Objects/Green_Bottle2.png"/>
+ </tile>
+ <tile id="21">
+  <image width="14" height="28" source="../../Graphics/Objects/Blue_Bottle1.png"/>
+ </tile>
+ <tile id="22">
+  <image width="24" height="14" source="../../Graphics/Objects/Blue_Bottle2.png"/>
+ </tile>
+ <tile id="23">
+  <image width="42" height="44" source="../../Graphics/Objects/BG_Barrel.png"/>
+ </tile>
+ <tile id="24">
+  <image width="42" height="44" source="../../Graphics/Objects/BG_Barrel_2.png"/>
+ </tile>
+ <tile id="25">
+  <image width="52" height="178" source="../../Graphics/Objects/Curtain1.png"/>
+ </tile>
+ <tile id="26">
+  <image width="52" height="178" source="../../Graphics/Objects/Curtain2.png"/>
+ </tile>
+ <tile id="27">
+  <image width="82" height="98" source="../../Graphics/Objects/Door.png"/>
+ </tile>
+</tileset>
