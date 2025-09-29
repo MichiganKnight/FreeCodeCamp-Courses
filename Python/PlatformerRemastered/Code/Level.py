16cedaca9def155a6a0dabf00ad29fee7242ff9d
Andrew Fleming <michiganknight2@gmail.com>
2025-09-29 16:57:52 -0500

    Moving Platforms

--- Python/PlatformerRemastered/Code/Level.py
+++ Python/PlatformerRemastered/Code/Level.py
@@ -1,7 +1,7 @@
 import pygame
 
 from Settings import *
-from Sprites import Sprite
+from Sprites import *
 from Player import Player
 
 class Level:
@@ -15,12 +15,29 @@ class Level:
         self.setup(tmx_map)
 
     def setup(self, tmx_map):
+        # Tiles
         for x, y, surface in tmx_map.get_layer_by_name("Terrain").tiles():
             Sprite((x * TILE_SIZE, y * TILE_SIZE), surface, (self.all_sprites, self.collision_sprites))
 
+        # Objects
         for obj in tmx_map.get_layer_by_name("Objects"):
             if obj.name == "Player":
                 Player((obj.x, obj.y), self.all_sprites, self.collision_sprites)
+
+        # Moving Objects
+        for obj in tmx_map.get_layer_by_name("Moving Objects"):
+            if obj.name == "Helicopter":
+                if obj.width > obj.height:
+                    move_direction = "X"
+                    start_position = (obj.x, obj.y + obj.height / 2)
+                    end_position = (obj.x + obj.width, obj.y + obj.height / 2)
+                else:
+                    move_direction = "Y"
+                    start_position = (obj.x + obj.width / 2, obj.y)
+                    end_position = (obj.x + obj.width / 2, obj.y + obj.height)
+
+                speed = obj.properties["Speed"]
+                MovingSprite((self.all_sprites, self.collision_sprites), start_position, end_position, move_direction, speed)
 
     def run(self, dt):
         self.all_sprites.update(dt)
--- Python/PlatformerRemastered/Code/Sprites.py
+++ Python/PlatformerRemastered/Code/Sprites.py
@@ -3,10 +3,51 @@ import pygame
 from Settings import *
 
 class Sprite(pygame.sprite.Sprite):
-    def __init__(self, position, surf, groups):
+    def __init__(self, position, surf = pygame.Surface((TILE_SIZE, TILE_SIZE)), groups = None):
         super().__init__(groups)
 
-        self.image = pygame.Surface((TILE_SIZE, TILE_SIZE))
+        self.image = surf
         self.image.fill("White")
         self.rect = self.image.get_rect(topleft = position)
         self.old_rect = self.rect.copy()
+
+class MovingSprite(Sprite):
+    def __init__(self, groups, start_position, end_position, move_direction, speed):
+        surf = pygame.Surface((200, 50))
+
+        super().__init__(start_position, surf, groups)
+
+        if move_direction == "X":
+            self.rect.midleft = start_position
+        else:
+            self.rect.midtop = start_position
+
+        self.start_position = start_position
+        self.end_position = end_position
+
+        # Movement
+        self.direction = vector(1, 0) if move_direction == "X" else vector(0, 1)
+        self.speed = speed
+        self.move_direction = move_direction
+
+    def check_border(self):
+        if self.move_direction == "X":
+            if self.rect.right >= self.end_position[0] and self.direction.x == 1:
+                self.direction.x = -1
+                self.rect.right = self.end_position[0]
+            if self.rect.left <= self.start_position[0] and self.direction.x == -1:
+                self.direction.x = 1
+                self.rect.left = self.start_position[0]
+        else:
+            if self.rect.bottom >= self.end_position[1] and self.direction.y == 1:
+                self.direction.y = -1
+                self.rect.bottom = self.end_position[1]
+            if self.rect.top <= self.start_position[1] and self.direction.y == -1:
+                self.direction.y = 1
+                self.rect.top = self.start_position[1]
+
+
+    def update(self, dt):
+        self.old_rect = self.rect.copy()
+        self.rect.topleft += self.direction * self.speed * dt
+        self.check_border()
