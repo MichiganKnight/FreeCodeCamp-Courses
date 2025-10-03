95ca211527a945b7f5e3539c23e2b2f192aecf9e
Andrew Fleming <michiganknight2@gmail.com>
2025-10-03 12:30:04 -0500

    Create Program.py

---
+++ Dev10/PythonBasics/FirstProject/Program.py
@@ -0,0 +1,14 @@
+import string
+
+print(1 + 1)
+print(0.1 + 0.2)
+print(10 * 1.1 - (5.5 + 0.1))
+print("Hello " + "World!")
+
+color = "red"
+
+print("A " + color + " Bus")
+print(string.ascii_letters)
+print(string.ascii_lowercase)
+print("Primary Colors: {0}, {1}, {2}".format(color, "Green", "Blue"))
+print("Hello World"[::-1])
