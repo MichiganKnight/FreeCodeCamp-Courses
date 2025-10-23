667a07260a3c40c1ae5d62c6ce9532fcd482ca3d
Andrew Fleming <michiganknight2@gmail.com>
2025-10-23 01:39:00 -0500

    Minor Updates

--- Web Development/python-server/app.js
+++ Web Development/python-server/app.js
@@ -12,15 +12,19 @@ let options = {
     args: []
 }
 
-PythonShell.run("TNA.py", options).then(messages => {
+PythonShell.run("TNA.py", options).then(async messages => {
     console.log("Python Script Finished");
     console.log("Results: ", messages);
 
     app.use(express.static(path.join(__dirname, "public")));
     app.use("/scripts", express.static(path.join(__dirname, "scripts")));
 
-    app.listen(port, () => {
-        console.log(`App Running at: 127.0.0.1:${port}`);
+    app.listen(port, async () => {
+        const serverUrl = `http://localhost:${port}`;
+        console.log(`Server Running at: ${serverUrl}`);
+
+        const open = (await import("open")).default;
+        await open(serverUrl);
     });
 }).catch(err => {
     console.error("Error Running Python Script: ", err);
