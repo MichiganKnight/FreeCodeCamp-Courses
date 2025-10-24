9b4bb5118bb574f13ee85b3df7c80678270c22c4
Andrew Fleming <michiganknight2@gmail.com>
2025-10-23 22:41:27 -0500

    Update app.js

--- Web Development/python-server/app.js
+++ Web Development/python-server/app.js
@@ -21,53 +21,48 @@ PythonShell.run("TNA.py", options).then(async messages => {
     console.log("Python TNA Script Finished");
     console.log("Results: ", messages);
 
-    PythonShell.run("Test.py", options).then(messages => {
-       console.log("Test Script Finished");
-       console.log("Results: ", messages);
+    PythonShell.run("SpankBang.py", options).then(async messages => {
+        console.log("Python SpankBang Script Finished");
+        console.log("Results: ", messages);
 
-        PythonShell.run("SpankBang.py", options).then(async messages => {
-            console.log("Python SpankBang Script Finished");
-            console.log("Results: ", messages);
+        app.set("view engine", "ejs");
+        app.set("views", path.join(__dirname, "views"));
+        app.use(expressLayouts);
+        app.set("layout", "layout");
 
-            app.set("view engine", "ejs");
-            app.set("views", path.join(__dirname, "views"));
-            app.use(expressLayouts);
-            app.set("layout", "layout");
+        app.use(express.static(path.join(__dirname, "public")));
+        app.use("/data", express.static(path.join(__dirname, "data")));
 
-            app.use(express.static(path.join(__dirname, "public")));
-            app.use("/data", express.static(path.join(__dirname, "data")));
+        app.get("/", (req, res) => {
+            res.redirect("/tna");
+        });
 
-            app.get("/", (req, res) => {
-                res.redirect("/tna");
+        app.get("/tna", (req, res) => {
+            const videosJSON = JSON.parse(fs.readFileSync(path.join(__dirname, "data/TNA.json"), "utf-8"));
+
+            res.render("videoPlayer", {
+                title: "TNA Videos",
+                heading: "TNA Videos",
+                videosJSON
             });
+        });
 
-            app.get("/tna", (req, res) => {
-                const videosJSON = JSON.parse(fs.readFileSync(path.join(__dirname, "data/TNA.json"), "utf-8"));
+        app.get("/spankbang", (req, res) => {
+            const videosJSON = JSON.parse(fs.readFileSync(path.join(__dirname, "data/SpankBang.json"), "utf-8"));
 
-                res.render("videoPlayer", {
-                    title: "TNA Videos",
-                    heading: "TNA Videos",
-                    videosJSON
-                });
+            res.render("videoPlayer", {
+                title: "SpankBang Videos",
+                heading: "SpankBang Videos",
+                videosJSON
             });
+        });
 
-            app.get("/spankbang", (req, res) => {
-                const videosJSON = JSON.parse(fs.readFileSync(path.join(__dirname, "data/SpankBang.json"), "utf-8"));
+        app.listen(port, async () => {
+            const serverUrl = `http://localhost:${port}`;
+            console.log(`Server Running at: ${serverUrl}`);
 
-                res.render("videoPlayer", {
-                    title: "SpankBang Videos",
-                    heading: "SpankBang Videos",
-                    videosJSON
-                });
-            });
-
-            app.listen(port, async () => {
-                const serverUrl = `http://localhost:${port}`;
-                console.log(`Server Running at: ${serverUrl}`);
-
-                const open = (await import("open")).default;
-                await open(serverUrl);
-            });
+            const open = (await import("open")).default;
+            await open(serverUrl);
         });
     });
 }).catch(err => {
