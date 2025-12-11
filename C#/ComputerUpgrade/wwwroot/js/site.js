381a9eadec9b56b314afc665f21662fe643d226c
Andrew Fleming <michiganknight2@gmail.com>
2025-12-11 04:43:12 -0600

    Images Spin

--- C#/ComputerUpgrade/wwwroot/js/site.js
+++ C#/ComputerUpgrade/wwwroot/js/site.js
@@ -1,4 +1,16 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿document.addEventListener("DOMContentLoaded", (event) => {
+    const image = document.getElementById("rotatingImage");
+    
+    if (image) {
+        const randomDirection = Math.random() < 0.5 ? 1 : -1;
+        
+        const minSpeed = 1;
+        const maxSpeed = 8;
+        const randomDuration = (Math.random() * (maxSpeed - minSpeed) + minSpeed).toFixed(2) + 's';
+        
+        console.log(`Image is Rotating: ${randomDirection} at ${randomDuration}`);
+        
+        image.style.setProperty("--rotation-direction", randomDirection);
+        image.style.setProperty("--rotation-duration", randomDuration)
+    }
+});
