a9eaa6297c3410e56cfab75d6fd7df965bf511b0
Andrew Fleming <michiganknight2@gmail.com>
2026-02-19 17:24:48 -0600

    Django Attempt

---
+++ Python/DjangoApp/Restaurant/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .forms import BookingForm
+#from .models import Menu
+
+def home(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def book(request):
+    form = BookingForm()
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            form.save()
+    context = {'form': form}
+    return render(request, 'book.html', context)
