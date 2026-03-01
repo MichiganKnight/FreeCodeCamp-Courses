a40422f88f4a763faf9ecb709b0dd71dfc1298ef
Andrew Fleming <michiganknight2@gmail.com>
2026-03-01 12:42:22 -0600

    Removed Games

--- Web Development/BasicPlatformer/JS/Editor/Editor.js
+++
@@ -1,5 +0,0 @@
-// Editor.js
-
-import { init } from "./Init.js";
-
-window.addEventListener("load", init);
--- Web Development/BasicPlatformer/JS/Index.js
+++
@@ -1,5 +0,0 @@
-const playButton = document.getElementById("play-button");
-const createButton = document.getElementById("create-button");
-
-playButton.addEventListener("click", () => window.location.href = "Game.html");
-createButton.addEventListener("click", () => window.location.href = "Editor.html");
